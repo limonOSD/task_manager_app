f000acb7d3be776f8fdacd9d65e65c763c5ee6a3
limon <limonpaulargo47@gmail.com>
2024-01-29 21:45:19 +0600

    int

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:task_manager_app/ui/screens/splash_screen.dart';
 
 class TaskManagerApp extends StatelessWidget {
@@ -7,24 +8,32 @@ class TaskManagerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       navigatorKey: navigationKey,
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
       theme: ThemeData(
-          inputDecorationTheme: const InputDecorationTheme(
-              fillColor: Colors.white,
-              filled: true,
-              border: OutlineInputBorder(borderSide: BorderSide.none),
-              focusedBorder: OutlineInputBorder(borderSide: BorderSide.none)),
-          textTheme: const TextTheme(
-              titleLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.w600)),
-          primaryColor: Colors.green,
-          primarySwatch: Colors.green,
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
+        inputDecorationTheme: const InputDecorationTheme(
+            fillColor: Colors.white,
+            filled: true,
+            border: OutlineInputBorder(borderSide: BorderSide.none),
+            focusedBorder: OutlineInputBorder(borderSide: BorderSide.none)),
+        textTheme: const TextTheme(
+            titleLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.w600)),
+        primaryColor: Colors.green,
+        primarySwatch: Colors.green,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
             padding: const EdgeInsets.symmetric(vertical: 12),
-          ))),
+          ),
+        ),
+      ),
+      initialBinding: ControllerBinder(),
     );
   }
+}
+
+class ControllerBinder extends Bindings {
+  @override
+  void dependencies() {}
 }
